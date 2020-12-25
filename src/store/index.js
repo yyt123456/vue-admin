d053cecd392eb5e4297d68c7990de32d1f26ae32
neuqsoft <yangyutong@neuqsoft.com>
2020-12-25 15:36:58 +0800

    feat: 新增vue-admin

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import Cookies from 'cookie_js'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    isCollapse: JSON.parse(Cookies.get('isCollapse')) || false
+    isCollapse: JSON.parse(sessionStorage.getItem('isCollapse')) || false
   },
   mutations: {
     SET_COLLAPSE(state) {
       state.isCollapse = !state.isCollapse;
-        Cookies.set('isCollapse', JSON.stringify(state.isCollapse))
+        sessionStorage.setItem('isCollapse', JSON.stringify(state.isCollapse))
     }
   },
   actions: {},
