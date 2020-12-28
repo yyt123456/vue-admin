d449ba675bb548666bbdd2de8689a15f9a3f8614
yyt123456 <2997353803@qq.com>
2020-12-28 08:42:47 +0800

    feat: 新增控制台

--- src/views/info/Category.vue
+++ src/views/info/Category.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
-    <el-button type="danger">分类</el-button>
+    <el-button type="danger" style="font-size: 14px">分类</el-button>
+    <div style="width: 100%;height: 1px;background-color: #ccc;margin: 20px 0"></div>
     <div>
       <el-row :gutter="30">
         <el-col :span="8">
           <div class="category-wrap">
             <div class="category">
-              <h4><svg-icon icon-class="plus"></svg-icon>新闻</h4>
+              <h4>
+                <svg-icon icon-class="plus"></svg-icon>
+                <span>新闻</span>
+                <div class="button-group">
+                  <el-button type="danger" size="mini">编辑</el-button>
+                  <el-button type="success" size="mini">添加子集</el-button>
+                  <el-button type="primary" size="mini">删除</el-button>
+                </div>
+              </h4>
               <ul>
-                <li>国内</li>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button type="danger" size="mini">编辑</el-button>
+                    <el-button type="primary" size="mini">删除</el-button>
+                  </div>
+                </li>
                 <li>国内</li>
                 <li>国内</li>
               </ul>
@@ -23,27 +38,61 @@
             </div>
           </div>
         </el-col>
-        <el-col :span="16"></el-col>
+        <el-col :span="16">
+          <h4 class="menu-title">一级分类编辑</h4>
+          <br>
+          <el-form
+            label-width="142px"
+            :model="formLabelAlign"
+            style="width: 410px"
+          >
+            <el-form-item label="一级分类名称">
+              <el-input v-model="formLabelAlign.name"></el-input>
+            </el-form-item>
+            <el-form-item label="二级分类名称">
+              <el-input v-model="formLabelAlign.region"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="danger">确定</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
       </el-row>
     </div>
   </div>
 </template>
 <script>
+import { ref, reactive, onMounted } from "@vue/composition-api";
 export default {
-  data() {
-    return {};
-  },
-  mounted() {},
-  methods: {}
+  setup() {
+    const formLabelAlign = reactive({
+      name: "",
+      region: "",
+      type: ""
+    });
+    onMounted(() => {});
+    return {
+      formLabelAlign
+    };
+  }
 };
 </script>
 <style lang="scss" scoped>
-  li, h4 {
-    &:hover{
-      @include webkit(transtion, all 0.3s ease 0);
-      background-color: #f3f3f3;
+.menu-title {
+  line-height: 44px;
+  background-color: #f3f3f3;
+  padding-left: 22px;
+}
+li,
+h4 {
+  &:hover {
+    @include webkit(transtion, all 0.3s ease 0);
+    background-color: #f3f3f3;
+    .button-group {
+      display: block;
     }
   }
+}
 .category-wrap {
   div:first-child {
     &:before {
@@ -89,6 +138,16 @@ export default {
       border-bottom: 1px dashed #000;
       left: 0;
       top: 22px;
+    }
+  }
+  .button-group {
+    position: absolute;
+    z-index: 2;
+    right: 11px;
+    top: 0;
+    display: none;
+    button {
+      font-size: 12px;
     }
   }
 }
